1c199701998bfd3ef87a3b35e32b1ee432dbe510
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-09 22:43:14 -0400

    Update cleanfiles.sh, install.sh, and pre-commit

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cd ${BASH_SOURCE%/*}
 
-cp .gitignore ../
 cp -a pre-commit ../.git/hooks/
 cp -a cleanfiles.sh ../
+mkdir ../../qaworkdir
+cp -a ./qa/trapdoor.cfg ../../qaworkdir
+cp -a ./qa/pycodestyle.ini ../../qaworkdir
